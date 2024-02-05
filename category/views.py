c62de93b5bbdaf3b4ec09e6f0a1f2bdba0281645
sarahyabdou <sarayasserma@gmail.com>
2024-02-05 14:38:20 +0200

    simple changes in  lab3

--- category/views.py
+++ category/views.py
@@ -5,22 +5,27 @@ from .forms  import *
 
 
 def categoryupdate(request,id):
-    # category=Category.objects.get(id=id)
-    category=Category.category_details(id)
+    category=Category.objects.get(id=id)
     context={'category':category}
+    
+    form=CategoryForm()
+    context={'form':form}
     if (request.method=='POST'):
-        if(request.POST['tname'] !='' and  request.POST['tdetails'] !=''):
+         form= CategoryForm(request,request.POST,request.FILES)
+       
+         if (form.is_valid):
+             obj1=Category.objects.get(id=id)
+             obj1.name=request.POST['name']
+                                    
+             obj1.details=request.POST['details']
+             obj1.numberproducts=request.POST['numberproducts']
+             obj1.image=request.FILES['image']
+           
+             obj1.save()
         
-            Category.objects.filter(id=id).update(name=request.POST['tname'],
-                                    
-                                    details=request.POST['tdetails'],
-                                     numberproducts=request.POST['tnum'],
-                                     
-                                    
-                                     )
-            r=reverse("category")
-            return HttpResponseRedirect(r)
-        else:
+             r=reverse("category")
+             return HttpResponseRedirect(r)
+         else:
             context['msg']='please,fill all fields'
     return render(request,'category/update.html',context)
 def category_view(request):
--- product/views.py
+++ product/views.py
@@ -8,20 +8,20 @@ from .forms  import *
 # def home(request):
 #     return HttpResponse("iti cairo university")
 
-def productupdate(request,id):
-    product=Product.product_details(id)
-    context={'product':product}
-    if (request.method=='POST'):
-        if(request.POST['tname'] !='' and  request.POST['tdetails'] !=''):
+# def productupdate(request,id):
+#     product=Product.product_details(id)
+#     context={'product':product}
+#     if (request.method=='POST'):
+#         if(request.POST['tname'] !='' and  request.POST['tdetails'] !=''):
         
-            Product.objects.filter(id=id).update(name=request.POST['tname'],
+#             Product.objects.filter(id=id).update(name=request.POST['tname'],
                                     
-                                    details=request.POST['tdetails'],)
-            r=reverse("product")
-            return HttpResponseRedirect(r)
-        else:
-            context['msg']='please,fill all fields'
-    return render(request,'product/update.html',context)
+#                                     details=request.POST['tdetails'],)
+#             r=reverse("product")
+#             return HttpResponseRedirect(r)
+#         else:
+#             context['msg']='please,fill all fields'
+#     return render(request,'product/update.html',context)
 
 def product_view(request):
     # Fetch product data from your desired source and organize it as a list
@@ -68,3 +68,30 @@ def addformproduct(request):
                 context['msg']='Data not compelete'
       return render(request,'formadd.html',context)   
   
+def productupdate(request,id):
+    product=Product.objects.get(id=id)
+    context={'product':product}
+    
+    form=ProductForm()
+    context={'form':form}
+    if (request.method=='POST'):
+         form= ProductForm(request,request.POST,request.FILES)
+       
+         if (form.is_valid):
+             obj1=Product.objects.get(id=id)
+             obj1.name=request.POST['name']
+                                    
+             obj1.details=request.POST['details']
+             obj1.image=request.FILES['image']
+             obj1.category=Category.objects.get(id=request.POST['category'])
+             obj1.save()
+            # Product.objects.filter(id=id).update(name=request.POST['name'],
+                                    
+            #                         details=request.POST['details'],
+            #                          image=request.FILES['image'],
+            #                                category=Category.objects.get(id=request.POST['category'],))
+             r=reverse("product")
+             return HttpResponseRedirect(r)
+         else:
+            context['msg']='please,fill all fields'
+    return render(request,'product/update.html',context)
